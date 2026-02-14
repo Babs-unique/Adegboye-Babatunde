4c66f7f6d7ef6e16ed19bfc03f3dfae7a9fda109
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-02-14 10:08:44 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/Adegboye-Babatunde",
+  base: process.env.VITE_BASE_PATH || "/Adegboye-Babatunde",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
